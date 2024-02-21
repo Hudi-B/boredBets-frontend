74ecc3c3e33607fa3d919b47fa1dc99f5d80640e
RRichard2004 <ricsiruszo90@gmail.com>
2024-02-21 18:00:14 +0100

    fix/admin page accesibility
    non-admin users cannot access admin page in any way

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import {BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import * as React from 'react';
 import './styles/Main.css';
-
 import Header from './Components/Header'
 import Home from './Pages/Home';
 import Community from './Pages/Community';
 import UserPage from './Pages/UserPage';
 import Races from './Pages/Races';
-
 import Admin from './Pages/Admin';
 
 
 
 function App() {
+  const [youAdmin , setYouAdmin] = React.useState(false);
+
   return (
   <div className='app'>
     <Router>
@@ -21,9 +22,7 @@ function App() {
         <Route path="*" element={<Home/>} />
         <Route exact path="/community" element={<Community/>} />
         <Route exact path="/races" element={<Races/>} />
-
-        <Route exact path="/admin" element={<Admin/>} />
-
+        {youAdmin && <Route exact path="/admin" element={<Admin/>} />}
         <Route exact path="/mypage" element={<UserPage/>} />
       </Routes>
     </Router>
