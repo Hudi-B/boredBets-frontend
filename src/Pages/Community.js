82e245aed301c75cb955bf60498a50d948a7cd00
RRichard2004 <ricsiruszo90@gmail.com>
2024-02-26 20:20:23 +0100

    fix/fixed axios request in community page
    now the axios function only sends 1 request and now its a .get instead of .post

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import config from '../config';
+import { useEffect } from 'react';
 export default function App() {
-  axios.post(`${config.apiUrl}user/getAllUsers`)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+
+  useEffect(() => {
+    axios.get(`${config.apiUrl}user/getAllUsers`)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+  },[])
+
   return (
     <div>
         Community
