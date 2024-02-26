6470e33210cd908d93452ca5eb3e145ec46af558
RRichard2004 <ricsiruszo90@gmail.com>
2024-02-26 20:17:26 +0100

    update/added testing content to community page
    added get all users to community page to test current api

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -1,5 +1,13 @@
+import axios from 'axios';
+import config from '../config';
 export default function App() {
-  
+  axios.post(`${config.apiUrl}user/getAllUsers`)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
   return (
     <div>
         Community
