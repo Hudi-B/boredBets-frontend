1980bf56f6d429b237b29a2fe7f85780736576c6
Hudi-B <vanosskanoss@gmail.com>
2024-02-27 10:08:47 +0100

    feature/added redux base

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
+import stores from './store/stores';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={stores}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
