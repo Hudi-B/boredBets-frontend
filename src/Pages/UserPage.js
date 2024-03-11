ae2d3a2cfabf350000421bc4de3231bf9fca3b4d
Hudi-B <vanosskanoss@gmail.com>
2024-03-11 23:07:23 +0100

    update/removed security.js

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Box, Paper, Chip, Stack } from '@mui/material';
+import { Box, Chip, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
 
 import Information from '../Components/UserComponents/Information';
-import Security from '../Components/UserComponents/Security';
 import Billing from '../Components/UserComponents/Billing';
 import History from '../Components/UserComponents/History';
 
@@ -15,7 +14,7 @@ const MenuChip = styled(Chip)(({ theme }) => ({
 
 export default function UserPage() {
 
-    const [currentPage, setCurrentPage] = React.useState('Information');
+    const [currentPage, setCurrentPage] = React.useState(0);
 
     const handleCompChange = (component) => {
         setCurrentPage(component);
@@ -36,17 +35,15 @@ export default function UserPage() {
             <Grid container spacing={5} sx={{width: '100%', height: '100%'}}>
                 <Grid item xs={4}>
                     <Stack spacing={2} sx={{ paddingTop: '10px' }}>
-                        <MenuChip label="Account Information" onClick={() => { handleCompChange('Information') }} />
-                        <MenuChip label="Account Security" onClick={() => { handleCompChange('Security') }} />
-                        <MenuChip label="Billing" onClick={() => { handleCompChange('Billing') }} />
-                        <MenuChip label="History" onClick={() => { handleCompChange('History') }} />
+                        <MenuChip label="Account Information" onClick={() => { handleCompChange(0) }} />
+                        <MenuChip label="Billing" onClick={() => { handleCompChange(1) }} />
+                        <MenuChip label="History" onClick={() => { handleCompChange(2) }} />
                     </Stack>
                 </Grid>
                 <Grid item xs={8}>
-                    {currentPage === 'Information' && <Information />}
-                    {currentPage === 'Security' && <Security />}
-                    {currentPage === 'Billing' && <Billing />}
-                    {currentPage === 'History' && <History />}
+                    {currentPage === 0 && <Information />}
+                    {currentPage === 1 && <Billing />}
+                    {currentPage === 2 && <History />}
                 </Grid>
             </Grid>
         </Box>
