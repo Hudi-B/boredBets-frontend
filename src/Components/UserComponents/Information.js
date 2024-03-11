1d238a987d144ac004efbadc81158122819823a9
Hudi-B <vanosskanoss@gmail.com>
2024-03-11 23:07:48 +0100

    update/added structure and base to info.js

--- src/Components/UserComponents/Information.js
+++ src/Components/UserComponents/Information.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Box, Paper } from '@mui/material';
+import { Avatar, Box, Paper, Stack, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
+
+const TilePaper = styled(Paper)(({ theme }) => ({
+    width: '100%',
+    boxShadow: theme.shadows[4],
+    backgroundColor: 'rgb(4, 112, 107)',
+    padding: theme.spacing(2),
+    textAlign: 'center',
+}))
 
 export default function Information() {
     return (
@@ -16,16 +25,26 @@ export default function Information() {
         >
             <Grid container spacing={2} sx={{width: '100%'}}>
                 <Grid item xs={4}>
-                    <Paper sx={{width: '100%', height: '100px', backgroundColor: 'rgb(2, 145, 138)'}}>asd</Paper>
+                    <Stack direction={'column'} spacing={2}>
+                        <TilePaper centered sx={{height: '250px', alignItems: 'center', justifyContent: 'center'}}>
+                            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                                <Avatar sx={{width: '100px', height: '100px', fontSize: '50px'}}>A</Avatar>
+                            </Box>
+                            <Typography variant="h5">UserID:</Typography>
+                            <Typography variant="h6">asrwh42-135fhsdhs3-qsdgdwsh</Typography>
+                        </TilePaper>
+                        <TilePaper sx={{height: '400px'}}>
+                            <Typography variant="h5">About you:</Typography>
+                        </TilePaper>
+                    </Stack>
                 </Grid>
                 <Grid item xs={8}>
-                    <Paper sx={{width: '100%', height: '100px', backgroundColor: 'rgb(2, 145, 138)'}}>asd</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper sx={{width: '100%', height: '100px', backgroundColor: 'rgb(2, 145, 138)'}}>asd</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper sx={{width: '100%', height: '100px', backgroundColor: 'rgb(2, 145, 138)'}}>asd</Paper>
+                    <Stack direction="column" spacing={2}>
+                        <TilePaper sx={{height: '100px'}}>Account status</TilePaper>
+                        <TilePaper sx={{height: '100px'}}>Email</TilePaper>
+                        <TilePaper sx={{height: '100px'}}>Password</TilePaper>
+                        <TilePaper sx={{height: '100px'}}>Preferences</TilePaper>
+                    </Stack>
                 </Grid>
             </Grid>
         </Box>
