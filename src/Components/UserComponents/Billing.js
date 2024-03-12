e4642dd5e89bdc51c58b4720f8745b77fa170c17
Hudi-B <vanosskanoss@gmail.com>
2024-03-12 21:22:02 +0100

    update/added fields to billing and history

--- src/Components/UserComponents/Billing.js
+++ src/Components/UserComponents/Billing.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { Box, Typography, Grid, Chip, Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const TilePaper = styled(Paper)(({ theme }) => ({
+    width: '100%',
+    boxShadow: theme.shadows[4],
+    backgroundColor: 'rgb(4, 112, 107)',
+    padding: theme.spacing(2),
+    textAlign: 'center',
+}))
 
 export default function Billing() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Typography variant="h4">Billing</Typography>
+        <Box sx={{ 
+            width: '100%',
+            display: 'flex', 
+            flexDirection: 'column', 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            padding: '10px', 
+            margin: '0px', 
+            overflowY: ''}}
+        >
+            <TilePaper sx={{ height: '600px' }}>
+                <Typography variant="h5">Manage your cards:</Typography>
+            </TilePaper>
         </Box>
     );
 }
--- src/Components/UserComponents/History.js
+++ src/Components/UserComponents/History.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid, Chip } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const MenuChip = styled(Chip)(({ theme }) => ({
+    height: '50px',
+    fontSize: '20px',
+    width: '100%'
+  }));
 
 export default function History() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Typography variant="h4">History</Typography>
+        <Box sx={{ 
+            width: '100%',
+            display: 'flex', 
+            flexDirection: 'column', 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            padding: '10px', 
+            margin: '0px', 
+            overflowY: ''}}
+           >
+            <Grid container spacing={2} sx={{width: '100%'}}>
+                <Grid item xs={6}>
+                    <MenuChip label="Betting history" onClick={() => {  }} />
+                </Grid>
+                <Grid item xs={6}>
+                    <MenuChip label="Billing history" onClick={() => {  }} />
+                </Grid>
+            </Grid>
         </Box>
     );
 }
