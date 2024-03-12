89bef3c2e9315b9bd443b77f4ac32d39cd7808f1
bag0ly <balazsforgo98@gmail.com>
2024-03-12 21:24:09 +0100

    update/added Filter
    wip

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -1,12 +1,27 @@
 import { apiUrl } from '../boredLocal';
-import { Box, Grid, TextField } from '@mui/material';
-import Autocomplete from '@mui/material/Autocomplete';
+import { Box, Grid, TextField,Autocomplete} from '@mui/material';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import '../styles/Community.css';
+import * as React from 'react';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+
 
 export default function App() {
   const [value, setValue] = useState(null);
   const [data, setData] = useState();
+
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
 
   useEffect(() => {
     axios.get(`${apiUrl}horse/getAllHorses`)
@@ -18,10 +33,26 @@ export default function App() {
       });
   }, []);
 
+
+  const [checked, setChecked] = React.useState([0]);//List
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
   return (
     <Box sx={{ py: 15, px: 5 }}>
       <Grid container spacing={2}>
-        <Grid xs={12} md={12} lg={12} sx={{ py: 1 }}>
+        <Grid item xs={12} md={12} lg={12} sx={{ py: 1 }}>
           <Autocomplete
             value={value}
             onChange={(event, newValue) => {
@@ -30,7 +61,7 @@ export default function App() {
                   title: newValue,
                 });
               } else if (newValue && newValue.inputValue) {
-                // Create a new value from the user input
+                
                 setValue({
                   title: newValue.inputValue,
                 });
@@ -42,31 +73,57 @@ export default function App() {
             clearOnBlur
             handleHomeEndKeys
             id="Searchbar"
-            options={data} // Use the data directly as options
+            options={data} 
             getOptionLabel={(option) => {
-              // Value selected with enter, right from the input
               if (typeof option === 'string') {
                 return option;
               }
-              // Add "xxx" option created dynamically
               if (option.inputValue) {
                 return option.inputValue;
               }
-              // Regular option
               return option.title;
             }}
             renderOption={(props, option) => <li {...props}>{option}</li>}
-            sx={{ width: 300 }}
+            sx={{ width: 'auto' }}
             freeSolo
             renderInput={(params) => (
               <TextField {...params} label="Search" />
             )}
           />
         </Grid>
-        <Grid xs={2} md={4} lg={2} sx={{ py: 1, px: 1 }}>
-          Filters
+        <Grid item xs={2} md={4} lg={2} sx={{ py: 1, px: 1 }}>
+
+              <Box className="container">
+                <Box className="animation-container">
+                    <Box className="animated-element"></Box>
+                    Mama
+                </Box>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                  {[0, 1, 2, 3].map((value) => {
+                    const labelId = `checkbox-list-label-${value}`;
+
+                    return (
+                      <ListItem key={value} disablePadding>
+                        <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+                          <ListItemIcon>
+                            <Checkbox
+                              edge="start"
+                              checked={checked.indexOf(value) !== -1}
+                              tabIndex={-1}
+                              disableRipple
+                              inputProps={{ 'aria-labelledby': labelId }}
+                            />
+                          </ListItemIcon>
+                          <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </Box>
+
         </Grid>
-        <Grid xs={10} md={8} lg={10} sx={{ py: 1, px: 1 }}>
+        <Grid item xs={10} md={8} lg={10} sx={{ py: 1, px: 1 }}>
           Items
         </Grid>
       </Grid>
