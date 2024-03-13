e5ee90cb3c40129fa047d1ec36c55b88f26a03b1
RRichard2004 <ricsiruszo90@gmail.com>
2024-03-13 18:11:46 +0100

    update/search bar refactoring
    website now fetches all possible data
    creates a searchValues array, which contains necessary data for search conditions,
    searchbar now shows all fetchable data

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -12,119 +12,101 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+import { faHorseHead, faHelmetSafety } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { keys } from '@mui/system';
+
+import PersonIcon from '@mui/icons-material/Person';
+
 
 export default function App() {
   const [value, setValue] = useState(null);
-  const [data, setData] = useState();
-
-  const [selectedValue, setSelectedValue] = useState('');
-
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
+  const [horses, setHorses] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [jockeys, setJockeys] = useState([]);
+  const [fetching, setFetching] = useState(true);
+  const [searchValues, setSearchValues] = useState([]);
 
   useEffect(() => {
-    axios.get(`${apiUrl}horse/getAllHorses`)
-      .then((response) => {
-        setData(response.data.map((horse) => horse.name));
-      })
-      .catch((error) => {
+    (async () => {
+      try {
+        const horseResponse = await axios.get(`${apiUrl}Horse/getAllHorses`);
+        setHorses(horseResponse.data);
+      
+        const jockeyResponse = await axios.get(`${apiUrl}Jockey/GetAllJockeys`);
+        setJockeys(jockeyResponse.data);
+      
+        /* Uncomment the following lines if you want to fetch users
+        const userResponse = await axios.get(`${apiUrl}User/GetAllUsers`);
+        setUsers(userResponse.data);
+        */
+      
+        let values = [
+          ...horseResponse.data.map((horse) => ({ 
+              type: <FontAwesomeIcon icon={faHorseHead}/>,
+              id: horse.id, 
+              name: horse.name, 
+              gender: horse.stallion ? <MaleIcon sx={{color:'blue'}}/> : <FemaleIcon sx={{color:'pink'}}/> 
+          })),
+          ...jockeyResponse.data.map((jockey) => ({
+              type: <FontAwesomeIcon icon={faHelmetSafety}/>, 
+              id: jockey.id,
+              name: jockey.name, 
+              gender: jockey.male ? <MaleIcon sx={{color:'blue'}}/> : <FemaleIcon sx={{color:'pink'}}/> 
+          })),
+          // Uncomment the following line if you want to fetch users
+          // ...userResponse.data.map((user) => ({ type: <PersonIcon />, id: user.id, name: user.name, gender: user.gender })),
+        ];
+      
+        values.sort((a, b) => a.name.localeCompare(b.name));
+        setSearchValues(values);
+
+        setFetching(false);
+      } catch (error) {
         console.log(error);
-      });
+      }
+    })();
   }, []);
-
-
-  const [checked, setChecked] = React.useState([0]);//List
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
 
   return (
     <Box sx={{ py: 15, px: 5 }}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={12} lg={12} sx={{ py: 1 }}>
-          <Autocomplete
-            value={value}
+            <Autocomplete
             onChange={(event, newValue) => {
               if (typeof newValue === 'string') {
                 setValue({
-                  title: newValue,
+                  name: newValue,
                 });
               } else if (newValue && newValue.inputValue) {
-                
                 setValue({
-                  title: newValue.inputValue,
+                  name: newValue.inputValue,
                 });
               } else {
                 setValue(newValue);
               }
             }}
             selectOnFocus
-            clearOnBlur
             handleHomeEndKeys
-            id="Searchbar"
-            options={data} 
+            options={searchValues}
             getOptionLabel={(option) => {
-              if (typeof option === 'string') {
-                return option;
-              }
-              if (option.inputValue) {
-                return option.inputValue;
-              }
-              return option.title;
+              return option.name;
             }}
-            renderOption={(props, option) => <li {...props}>{option}</li>}
+            renderOption={(props, option) => 
+            <li {...props}key={option.id}>
+              {option.type}
+              {option.name}
+              {option.gender}
+            </li>
+          }
             sx={{ width: 'auto' }}
-            freeSolo
             renderInput={(params) => (
               <TextField {...params} label="Search" />
             )}
           />
-        </Grid>
-        <Grid item xs={2} md={4} lg={2} sx={{ py: 1, px: 1 }}>
-
-              <Box className="container">
-                <Box className="animation-container">
-                    <Box className="animated-element"></Box>
-                    Mama
-                </Box>
-                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                  {[0, 1, 2, 3].map((value) => {
-                    const labelId = `checkbox-list-label-${value}`;
-
-                    return (
-                      <ListItem key={value} disablePadding>
-                        <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-                          <ListItemIcon>
-                            <Checkbox
-                              edge="start"
-                              checked={checked.indexOf(value) !== -1}
-                              tabIndex={-1}
-                              disableRipple
-                              inputProps={{ 'aria-labelledby': labelId }}
-                            />
-                          </ListItemIcon>
-                          <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-                        </ListItemButton>
-                      </ListItem>
-                    );
-                  })}
-                </List>
-              </Box>
-
-        </Grid>
-        <Grid item xs={10} md={8} lg={10} sx={{ py: 1, px: 1 }}>
-          Items
         </Grid>
       </Grid>
     </Box>
