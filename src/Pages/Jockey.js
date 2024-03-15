3f8e26c8bc0311e6427a895b65801e0dda620a35
RRichard2004 <ricsiruszo90@gmail.com>
2024-03-15 21:06:08 +0100

    feature/ Added single view pages
    page gets the data of given id from correct endpoint and stores it

---
+++ src/Pages/Jockey.js
@@ -0,0 +1,30 @@
+import { Box } from '@mui/material';
+import { useEffect } from 'react';
+import {useLocation} from 'react-router-dom';
+import { apiUrl } from '../boredLocal';
+import axios from 'axios';
+import { useState } from 'react';
+
+
+export default function App() {
+  const id = useLocation().pathname.split("/")[2];
+  const [data, setData] = useState({});
+  console.log(id);
+
+  useEffect(() => {
+    axios.get(`${apiUrl}Jockey/GetJockeyById?JockeyId=${id}`)
+    .then((response) => {
+        setData(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+  }, []);
+
+  return (
+    <Box>
+      
+    </Box>
+  );
+}
+  
