6ecdbdef2d42589976f1a18e6b1142a8b7eece28
RRichard2004 <ricsiruszo90@gmail.com>
2024-03-20 09:43:41 +0100

    update/ header on individual page
    name, gender icon, 2 number info is ready to display

--- src/Pages/Jockey.js
+++ src/Pages/Jockey.js
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography, Chip } from '@mui/material';
 import { useEffect } from 'react';
 import {useLocation} from 'react-router-dom';
 import { apiUrl } from '../boredLocal';
@@ -29,7 +29,7 @@ export default function App() {
         backgroundColor: 'rgb(4, 112, 107)',
         borderTopRightRadius: '20px',
         borderTopLeftRadius: '20px',
-        height:'100%',
+        minHeight:'100%',
         width:'90%',
         maxWidth: '1000px',
         marginTop: '100px',
@@ -38,11 +38,45 @@ export default function App() {
       }}>
 
         <Stack direction="collumn">
-          <Stack direction={"row"}>
-            
+          <Stack className='preventSelect' direction={"row"} sx={{margin: '20px',width: '100%', gap: 1 }}>
+            <Box id="insteadOfPfp" sx={{
+                width: '200px',
+                minWidth: '200px',
+                height: '200px',
+                borderRadius: '50%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '30px',
+                backgroundColor: 'white',
+            }}>pfp FR</Box>
+            <Stack direction={"column"} sx={{
+              width: '100%',
+              height: '100%',
+            }}>
+              <Box sx={{paddingTop: '20px', width: '100%', display: 'flex', justifyContent: 'space-between'}}>
+                <Typography variant='h5'>Individuals name FR</Typography>
+                <Typography variant='h5'>Icon</Typography>
+              </Box>
+              <Box sx={{width: '100%', marginY: 'auto',display: 'flex', justifyContent: 'space-between', paddingX: '20px'}}>
+              <Stack sx={{alignItems:'flex-end'}}>
+                <Typography>
+                  Explain uno:
+                </Typography>
+              <Chip sx={{paddingX: '10px', fontSize: '15px'}} label="123"/>
+              </Stack>
+
+              <Stack sx={{alignItems:'flex-end'}}>
+                <Typography>
+                  Explain dos:
+                </Typography>
+              <Chip sx={{paddingX: '10px', fontSize: '15px'}} label="456"/>
+              </Stack>
+              </Box>
+
+            </Stack>
           </Stack>
         </Stack>
-      
     </Box>
   );
 }
