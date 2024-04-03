0aa821ca141fae8ae5407a8ee9d028a7a4b5b9db
Hudi-B <vanosskanoss@gmail.com>
2024-04-03 21:18:40 +0200

    update/added notifications for card actions

--- src/Components/UserComponents/Cards.js
+++ src/Components/UserComponents/Cards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Box, Typography, Grid, Chip, Paper, Stack, Card, Dialog, DialogContent, DialogActions, DialogTitle } from "@mui/material";
+import { Box, Typography, Grid, Chip, Paper, Stack, Card, Dialog, DialogContent, DialogActions, DialogTitle, Input, InputAdornment, Select } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import CardPaper from "./CardPaper";
 import AddIcon from '@mui/icons-material/Add';
@@ -8,6 +8,8 @@ import axios from "axios";
 import { apiUrl } from '../../boredLocal';
 import CreditCardForm from "./CreditCardForm";
 import { useSelector } from 'react-redux';
+import { useSnackbar } from 'notistack';
+import Slide from '@mui/material/Slide';
 
 const TilePaper = styled(Paper)(({ theme }) => ({
     width: '100%',
@@ -24,6 +26,7 @@ export default function Cards() {
     const [isLoading, setIsLoading] = useState(true);
     const userId = useSelector((state) => state.auth.userId);
     const [open, setOpen] = useState(false);
+    const { enqueueSnackbar } = useSnackbar();
 
     const handleOpen = () => {
         setOpen(true);
@@ -51,7 +54,8 @@ export default function Cards() {
 
     const handleDelete = async (cardId) => {
         axios.delete(apiUrl+`UserCard/DeleteByCreditCardNum?CreditCardNum=` + cardId)
-        .then((response) => {
+        .then(() => {
+            enqueueSnackbar("Card Deleted", { variant: 'error', autoHideDuration: 3000, TransitionComponent: Slide, });
             fetchData();
             setIsLoading(false);
         })
@@ -71,7 +75,25 @@ export default function Cards() {
             margin: '0px', 
             overflowY: ''}}
         >
-            <Stack direction={'column'} spacing={2} sx={{ width: '100%', alignItems: 'center' }}>
+            <Stack direction={'column'} spacing={1} sx={{ width: '100%', alignItems: 'center' }}>
+                <TilePaper>
+                    <Stack direction={'row'} justifyContent={'space-between'}>
+                        <Typography variant="h5">Deposit:</Typography>
+                        <Chip label="Finish" icon={<AddIcon style={{ color: 'white' }} />} style={{ color: 'white' }} onClick={() => {}} />
+                    </Stack>
+                    <Box sx={{ padding: '35px', justifyContent: 'center', display: 'flex', color: 'white' }}>
+                        <Stack direction={'row'} spacing={5}>
+                            <Stack direction={'column'} spacing={1}>
+                                <Typography variant="caption">Amount</Typography>
+                                <Input placeholder="Amount" startAdornment={<InputAdornment position="start">$</InputAdornment>} />
+                            </Stack>
+                            <Stack direction={'column'} spacing={1}>
+                                <Typography variant="caption">Card</Typography>
+                                <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Card" />
+                            </Stack>
+                        </Stack>
+                    </Box>
+                </TilePaper>
                 <TilePaper>
                     <Stack direction={'row'} justifyContent={'space-between'}>
                         <Typography variant="h5">Manage your cards:</Typography>
