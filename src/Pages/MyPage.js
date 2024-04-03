bc95e65f02cd9442b7d3e13a535be1ebc913079a
Hudi-B <vanosskanoss@gmail.com>
2024-04-03 21:16:57 +0200

    update/styled userpage nav buttons

--- src/Pages/MyPage.js
+++ src/Pages/MyPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Chip, Stack } from '@mui/material';
+import { Box, Chip, Divider, Stack, Typography, Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
 
@@ -23,19 +23,25 @@ export default function UserPage() {
 
     return (
         <Box sx={{ 
-         padding: '10px', 
+         padding: '35px', 
          paddingTop: '50px' }}
          
         >
             <Grid container spacing={5} sx={{width: '100%', height: '100%'}}>
-                <Grid item xs={4}>
-                    <Stack spacing={2} sx={{ paddingTop: '10px' }}>
-                        <MenuChip label="Account" onClick={() => { handleCompChange(0) }} />
-                        <MenuChip label="Payment" onClick={() => { handleCompChange(1) }} />
-                        <MenuChip label="History" onClick={() => { handleCompChange(2) }} />
+                <Grid item xs={2}>
+                    <Stack spacing={2} alignItems={'center'} className='preventSelect' sx={{ paddingTop: '10px' }}>
+                        <Paper elevation={ currentPage === 0 && 5} square sx={{ padding: '10px', backgroundColor: 'transparent', minWidth: '250px'}} onClick={() => { handleCompChange(0) }} >
+                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' > Account </Typography>
+                        </Paper>
+                        <Paper elevation={ currentPage === 1 && 5} square sx={{ padding: '10px', backgroundColor: 'transparent', minWidth: '250px'}} onClick={() => { handleCompChange(1) }} >
+                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' > Payment </Typography>
+                        </Paper>
+                        <Paper elevation={ currentPage === 2 && 5} square sx={{ padding: '10px', backgroundColor: 'transparent', minWidth: '250px'}} onClick={() => { handleCompChange(2) }} >
+                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' > History </Typography>
+                        </Paper>
                     </Stack>
                 </Grid>
-                <Grid item xs={8} sx={{minHeight: '100vh'}}>
+                <Grid item xs={10} sx={{minHeight: '100vh'}}>
                     {currentPage === 0 && <Information />}
                     {currentPage === 1 && <Cards />}
                     {currentPage === 2 && <History />}
