389dadee88c459ba8a3d2ba8af953e53c74f215d
Hudi-B <vanosskanoss@gmail.com>
2024-04-05 00:18:02 +0200

    fix/height and text hotfix

--- src/Pages/MyPage.js
+++ src/Pages/MyPage.js
@@ -18,24 +18,24 @@ export default function UserPage() {
     return (
         <Box sx={{ 
          padding: '35px', 
-         paddingTop: '50px' }}
-         
-        >
+         paddingTop: '50px',
+         minHeight: '100vh', }}
+         >
             <Grid container spacing={5} sx={{width: '100%', height: '100%'}}>
                 <Grid item xs={2}>
                     <Stack spacing={2} alignItems={'center'} className='preventSelect' sx={{ paddingTop: '10px' }}>
                         <Paper elevation={ currentPage === 0 ? 5 : 0} square sx={{ padding: '10px', backgroundColor: 'transparent', minWidth: '250px'}} onClick={() => { handleCompChange(0) }} >
-                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' > Account </Typography>
+                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' >Account</Typography>
                         </Paper>
                         <Paper elevation={ currentPage === 1 ? 5 : 0} square sx={{ padding: '10px', backgroundColor: 'transparent', minWidth: '250px'}} onClick={() => { handleCompChange(1) }} >
-                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' > Payment </Typography>
+                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' >Payment</Typography>
                         </Paper>
                         <Paper elevation={ currentPage === 2 ? 5 : 0} square sx={{ padding: '10px', backgroundColor: 'transparent', minWidth: '250px'}} onClick={() => { handleCompChange(2) }} >
-                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' > History </Typography>
+                            <Typography sx={{ color: 'white', paddingLeft: '10px' }} variant='h5' >History</Typography>
                         </Paper>
                     </Stack>
                 </Grid>
-                <Grid item xs={10} sx={{minHeight: '100vh'}}>
+                <Grid item xs={10}>
                     {currentPage === 0 && <Information />}
                     {currentPage === 1 && <Cards />}
                     {currentPage === 2 && <History />}
