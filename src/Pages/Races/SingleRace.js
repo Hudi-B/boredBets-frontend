16c6e74b56ab04649d2a2c0fdb4813d54704b812
RRichard2004 <ricsiruszo90@gmail.com>
2024-04-07 21:52:26 +0200

    update/tempomary Drag and drop added
    installed beautiful-dnd

--- src/Pages/Races/SingleRace.js
+++ src/Pages/Races/SingleRace.js
@@ -1,15 +1,75 @@
-import { useLocation} from "react-router-dom";
-import { useEffect, useState } from 'react';
-
-export default function SingleRace() {
-    const id = useLocation().pathname.split("/")[2];
-    const [raceData , setRaceData] = useState({});
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import '../../styles/DnD.css';
 
 
+
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+  },
+  {
+    id: 'mooncake',
+    name: 'Mooncake',
+  },
+  {
+    id: 'quinn',
+    name: 'Quinn Ergon'
+  }
+]
+
+function App() {
+  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+    console.log(items.map((char) => char.name));
+  }
 
   return (
-    <>
-      <h1>Single Race - {id}</h1>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Horses drag and drop</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                {characters.map(({id, name}, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <p>
+                            { name }
+                          </p>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </header>
+    </div>
   );
 }
+
+export default App;
