f6cd2a76780c48a449770c1be57e343ee51b7192
RRichard2004 <ricsiruszo90@gmail.com>
2024-04-10 12:50:28 +0200

    update/single race page developed further

--- src/Pages/Races/SingleRace.js
+++ src/Pages/Races/SingleRace.js
@@ -4,11 +4,12 @@ import '../../styles/DnD.css';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { apiUrl } from '../../boredLocal';
-import {Stack, Divider, Grid, Box, Typography, Button, Hidden} from "@mui/material";
+import {Stack,Tooltip, Divider, Grid, Box, Typography, Button, Hidden} from "@mui/material";
 import { useLocation } from 'react-router-dom';
 import CloudIcon from '@mui/icons-material/Cloud';
 import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
 import StraightenIcon from '@mui/icons-material/Straighten';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
 function App() {
   const id = useLocation().pathname.split("/")[2];
 
@@ -16,9 +17,9 @@ function App() {
   const [participants, setParticipants] = useState([]);
   const [pending, setPending] = useState(true);
 
+
   useEffect(() => {
-    axios
-      .get(`${apiUrl}Race/GetByRaceId?Id=`+id)
+    axios.get(`${apiUrl}Race/GetByRaceId?Id=`+id)
       .then((response) => {
         setRace({
           raceId: response.data.raceId,
@@ -34,53 +35,93 @@ function App() {
       });
   }, []);
 
+  const handleMapOpen = () => {
+    if (!pending) {
+      const searchString = `${race.track.country}, ${race.track.address}`; // Replace with your desired string
+      const mapUrl = `https://www.google.com/maps/search/?q=${searchString}`;
+      window.open(mapUrl, '_blank');
+    }
+  };
+
   return (
     <Box>
-      <Grid container sx={{marginBottom: 2, marginTop: 10, gap: 2, justifyContent: 'center'}}>
-        <Grid item xs={12} sm={12} md={5}
+      <Grid container 
         sx={{
-          display: 'flex',
-         justifyContent: 'center',
-         alignItems: 'center',
-         fontSize: 'calc(1.5rem + 1.5vw)',
-         whiteSpace: 'nowrap',
-         letterSpacing: '0.1em',
-         fontWeight: '500',         
-          }}>
-            {pending? <div>Loading...</div> : race.track.name}
-        </Grid>
+          marginBottom: 2, 
+          gap: 2, 
+          justifyContent: 'center',
+          paddingTop: 10,
+          backgroundColor: 'rgba(50,50,50,0.3)'}}>
+            <Grid item xs={12} sm={12} md={5}
+            sx={{
+              display: 'flex',
+              textAlign: 'center',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: 'calc(1.5rem + 1.5vw)',
+              letterSpacing: '0.1em',
+              fontWeight: '500'}}>
+                {pending? <div>Loading...</div> : race.track.name}
+            </Grid>
 
-        <Grid item xs={12} sm={12} md={5}
+            <Grid item xs={12} sm={12} md={5}
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 'calc(1.2rem + 2vw)',
+                whiteSpace: 'nowrap'}}>
+                {pending? 
+                <div>Loading...</div>/*skeleton here*/
+                : 
+                "Race held at: "+race.raceSceduled.replace("T"," ").replace("Z","").replaceAll("-", "/")
+                }
+            </Grid>
+            <Stack
+              direction="row"
+              sx={{
+              width: '100%',  
+              fontSize: 'calc(0.9rem + 1.5vw)',
+              whiteSpace: 'nowrap',
+              justifyContent: 'space-between',
+              alignItems: 'space-between',
+              letterSpacing: '0.1em',    
+              marginX:3 
+                }}>
+                  {pending? 
+                <div>Loading...</div>/*skeleton here*/
+                : <p>Weather: {pending? <div>Loading...</div> : race.rain?<CloudIcon/> : <WbSunnyRoundedIcon/>}  </p>}
+                
+                {pending? 
+                  <div>Loading...</div>/*skeleton here*/
+                  : <p><StraightenIcon /> {race.track.length}km</p>}
+            </Stack>
+
+      </Grid>
+
+
+        <Stack
+          direction="row"
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: 'calc(1.2rem + 2vw)',
-            whiteSpace: 'nowrap'}}>
-            {pending? 
-            <div>Loading...</div>/*skeleton here*/
-            : 
-            race.raceSceduled
-            }
-        </Grid>
-        <Box
-          sx={{
-            display: 'flex',
-          fontSize: 'calc(0.9rem + 1.5vw)',
-          whiteSpace: 'nowrap',
-          justifyContent: 'space-between',
+          width: '100%',  
+          fontSize: '13px',
+          justifyContent: 'flex-end',
           alignItems: 'center',
           letterSpacing: '0.1em',    
-          marginX:3 
-            }}>
-              {pending? 
-            <div>Loading...</div>/*skeleton here*/
-            : <p>Weather: {pending? <div>Loading...</div> : race.rain?<CloudIcon/> : <WbSunnyRoundedIcon/>}  </p>}
+          paddingX:1 }}>
             {pending? 
-              <div>Loading...</div>/*skeleton here*/
-              : <p><StraightenIcon /> {race.track.length}km</p>}
-        </Box>
-      </Grid>
+                <div>Loading...</div>/*skeleton here*/
+                : <>{race.track.address}</>}
+         
+          <Tooltip title="Show on map" placement='top'>
+            <Button variant="string" onClick={handleMapOpen}
+            sx={{width: 'fit-content'}}>
+              <FmdGoodIcon sx={{fontSize: '40px', margin:'none'}}/>
+            </Button>
+          </Tooltip>
+        </Stack>
+
+          <Typography sx={{marginLeft: 2}} variant="h5">In the competition:</Typography>
 
     </Box>
   );
