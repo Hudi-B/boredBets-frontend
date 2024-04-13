fae6d5bdd1a5d1e936d2ded27914371e3868915d
Hudi-B <vanosskanoss@gmail.com>
2024-04-13 16:05:18 +0200

    update/moved dialog into component

--- src/Components/UserComponents/UserDetailForm.js
+++ src/Components/UserComponents/UserDetailForm.js
@@ -1,53 +1,54 @@
 import React from 'react';
 import { useState } from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
-import { Box, Button, Divider, Grid, Input, MenuItem, Select, Stack, Typography, FormControl } from '@mui/material';
+import { Box, Button, Divider, Grid, Input, MenuItem, Select, Stack, Typography, FormControl, Dialog } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import Slide from '@mui/material/Slide';
 import { phonePrefixes } from '../../boredLocal';
 
-const UserDetailForm = ( onClose, onSubmit ) => {
+const UserDetailForm = ( {open, onClose, onSubmit} ) => {
     const { enqueueSnackbar } = useSnackbar();
     const [phonePrefix, setPhonePrefix] = useState('+36');
 
     return (
-            <Box>
+            <Dialog open={open} onClose={onClose}>
                 <Box>
-                    <ClearIcon style={{ color: 'white', position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }} onClick={onClose} />
-                </Box>
-                <Box sx={{ padding: '50px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', backgroundColor: 'rgb(4, 112, 107)' }}>
-                    <Typography variant='h4' sx={{ color: 'white', paddingBottom: '40px' }}>About you</Typography>
-                    <Stack spacing={2} alignItems={'center'} direction={'column'}>
-                        <Stack spacing={1} direction={'column'} alignItems={'start'}>
-                            <Typography variant='subtitle2' sx={{ color: 'white' }}>Name</Typography>
-                            <Input sx={{ width: '300px', color: 'white' }} placeholder='Name' />
-                        </Stack>
-                        <Stack spacing={1} direction={'column'} alignItems={'start'}>
-                            <Typography variant='subtitle2' sx={{ color: 'white' }}>Date of birth</Typography>
-                            <Input sx={{ width: '300px', color: 'white' }} placeholder='Date of birth' />
-                        </Stack>
-                        <Stack spacing={1} direction={'column'} alignItems={'start'}>
-                            <Typography variant='subtitle2' sx={{ color: 'white' }}>Phone number</Typography>
-                            <Stack direction={'row'}>
-                                <FormControl>
-                                    <Select variant='standard' value={phonePrefix}>
-                                        {phonePrefixes.map((prefix) =>
-                                            <MenuItem key={prefix} value={prefix}>{prefix}</MenuItem>
-                                            )}
-                                        <MenuItem value={'+44'}>+44</MenuItem>
-                                    </Select>
-                                </FormControl>
-                                <Input sx={{ width: '300px', color: 'white' }} placeholder='Phone number' />
+                    <Box>
+                        <ClearIcon style={{ color: 'white', position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }} onClick={onClose} />
+                    </Box>
+                    <Box sx={{ padding: '50px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', backgroundColor: 'rgb(4, 112, 107)' }}>
+                        <Typography variant='h4' sx={{ color: 'white', paddingBottom: '40px' }}>About you</Typography>
+                        <Stack spacing={2} alignItems={'center'} direction={'column'}>
+                            <Stack spacing={1} direction={'column'} alignItems={'start'}>
+                                <Typography variant='subtitle2' sx={{ color: 'white' }}>Name</Typography>
+                                <Input sx={{ width: '300px', color: 'white' }} placeholder='Name' />
                             </Stack>
+                            <Stack spacing={1} direction={'column'} alignItems={'start'}>
+                                <Typography variant='subtitle2' sx={{ color: 'white' }}>Date of birth</Typography>
+                                <Input sx={{ width: '300px', color: 'white' }} placeholder='Date of birth' />
+                            </Stack>
+                            <Stack spacing={1} direction={'column'} alignItems={'start'}>
+                                <Typography variant='subtitle2' sx={{ color: 'white' }}>Phone number</Typography>
+                                <Stack direction={'row'}>
+                                    <FormControl>
+                                        <Select variant='standard' value={phonePrefix} onChange={(e) => setPhonePrefix(e.target.value)}>
+                                            {phonePrefixes.map((prefix) =>
+                                                <MenuItem key={prefix} value={prefix}>{prefix}</MenuItem>
+                                                )}
+                                        </Select>
+                                    </FormControl>
+                                    <Input sx={{ width: '300px', color: 'white' }} placeholder='Phone number' />
+                                </Stack>
+                            </Stack>
+                            <Stack spacing={1} direction={'column'} alignItems={'start'}>
+                                <Typography variant='subtitle2' sx={{ color: 'white' }}>Address</Typography>
+                                <Input sx={{ width: '300px', color: 'white' }} placeholder='Adress' />
+                            </Stack>
+                            <Button variant='contained' sx={{ padding: '10px', width: '300px', color: 'white', backgroundColor: 'rgb(4, 112, 107)' }} onClick={onSubmit}>Submit</Button>
                         </Stack>
-                        <Stack spacing={1} direction={'column'} alignItems={'start'}>
-                            <Typography variant='subtitle2' sx={{ color: 'white' }}>Address</Typography>
-                            <Input sx={{ width: '300px', color: 'white' }} placeholder='Adress' />
-                        </Stack>
-                        <Button variant='contained' sx={{ padding: '10px', width: '300px', color: 'white', backgroundColor: 'rgb(4, 112, 107)' }} onClick={onSubmit}>Submit</Button>
-                    </Stack>
+                    </Box>
                 </Box>
-            </Box>
+            </Dialog>
     )
 }
 
