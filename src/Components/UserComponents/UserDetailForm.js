cc31666f34c1a17a3a08fa22333057c846dcb1d1
Hudi-B <vanosskanoss@gmail.com>
2024-04-26 20:47:07 +0200

    update/finished inputs for userdetail form

--- src/Components/UserComponents/UserDetailForm.js
+++ src/Components/UserComponents/UserDetailForm.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import { useState } from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
-import { Box, Button, Divider, Grid, Input, MenuItem, Select, Stack, Typography, FormControl, Dialog } from '@mui/material';
+import { Box, Button, Divider, Grid, Input, MenuItem, Select, Stack, Typography, FormControl, Dialog, DialogContent, DialogContentText, TextField} from '@mui/material';
 import { useSnackbar } from 'notistack';
+import { styled } from '@mui/material/styles';
 import Slide from '@mui/material/Slide';
-import { phonePrefixes } from '../../boredLocal';
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
-const customPhoneInputStyle = {
-    color: 'white',
-    backgroundColor: 'transparent',
-    border: 'none',
-    borderBottom: '1px solid black',
-    borderRadius: 0,
-  };
-
-const customDatePickerStyle = {
-    color: 'white',
-    backgroundColor: 'transparent',
-    border: 'none',
-    borderBottom: '1px solid black',
-    borderRadius: 0,
-};
+const CustomInput = styled(TextField)({
+    input: {
+      color: 'white',
+      fontSize: '16px',
+      width: '300px',
+    },
+  });
 
 const UserDetailForm = ( {open, onClose, onSubmit} ) => {
     const { enqueueSnackbar } = useSnackbar();
     const [phoneNumber, setPhoneNumber] = useState('');
     const [name, setName] = useState('');
-    const [dateOfBirth, setDateOfBirth] = useState(new Date());
+    const [dateOfBirth, setDateOfBirth] = useState(dayjs());
     const [address, setAddress] = useState('');
 
     return (
@@ -44,20 +37,21 @@ const UserDetailForm = ( {open, onClose, onSubmit} ) => {
                         <Stack spacing={2} alignItems={'center'} direction={'column'}>
                             <Stack spacing={1} direction={'column'} alignItems={'start'}>
                                 <Typography variant='subtitle2' sx={{ color: 'white' }}>Name</Typography>
-                                <Input sx={{ width: '300px', color: 'white' }} placeholder='Name' />
+                                <CustomInput variant='standard' placeholder='John Doe' />
                             </Stack>
                             <Stack spacing={1} direction={'column'} alignItems={'start'}>
                                 <Typography variant='subtitle2' sx={{ color: 'white' }}>Date of birth</Typography>
-                                <DatePicker {...customDatePickerStyle} style={{ width: '100%' }} selected={dateOfBirth} onChange={(date) => setDateOfBirth(date)} dateFormat="MM-dd-yyyy"/>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker slotProps={{ textField: { variant: 'standard', style:{ width: '300px' }, inputProps: { style: { color: 'white', fontSize: '16px' }, } }}} slots={{ openPickerIcon: () => <CalendarMonthIcon style={{ fontSize: '16px', color: 'white' }} />,}} value={dateOfBirth} onChange={(newValue) => { setDateOfBirth(newValue) }} />
+                                </LocalizationProvider>
                             </Stack>
                             <Stack spacing={1} direction={'column'} alignItems={'start'}>
                                 <Typography variant='subtitle2' sx={{ color: 'white' }}>Phone number</Typography>
-                                <PhoneInput country={'us'} disableDropdown inputStyle={customPhoneInputStyle} buttonStyle={customPhoneInputStyle} value={phoneNumber} onChange={(value) => setPhoneNumber(value)}
-                                />
+                                <CustomInput variant='standard' placeholder='+36012345678' />
                             </Stack>
                             <Stack spacing={1} direction={'column'} alignItems={'start'}>
                                 <Typography variant='subtitle2' sx={{ color: 'white' }}>Address</Typography>
-                                <Input sx={{ width: '300px', color: 'white' }} placeholder='Adress' />
+                                <CustomInput variant='standard' placeholder='Palóczy László utca 3, 3525' />
                             </Stack>
                             <Button variant='contained' sx={{ padding: '10px', width: '300px', color: 'white', backgroundColor: 'rgb(4, 112, 107)' }} onClick={onSubmit}>Submit</Button>
                         </Stack>
